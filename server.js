9eb4859744380ad4f576eac8bd995f94af56891b
LeventDemir <leventdemir518@gmail.com>
2019-05-22 13:57:53 +0300

    ready for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://levent:Levent.1234@ds159546.mlab.com:59546/musichub", {
   useNewUrlParser: true,
@@ -13,23 +13,21 @@ mongoose.connect("mongodb://levent:Levent.1234@ds159546.mlab.com:59546/musichub"
 });
 
 mongoose.connection.on("open", () => console.log("Connected to mongodb"));
-mongoose.connection.on("error", err =>
-  console.log(`Mongodb connection error: ${err}`)
-);
+mongoose.connection.on("error", err => console.log(`Mongodb connection error: ${err}`));
+
 
 app.use(bodyParser.json({ limit: "200mb" }));
 app.use(cors());
 
-app.use("/user", require("./routes/user"));
-app.use("/playlist", require("./routes/playlist"));
-app.use("/music", require("./routes/music"));
-app.use("/public", require("./routes/public"));
+app.use("/user", require("./src/routes/user"));
+app.use("/playlist", require("./src/routes/playlist"));
+app.use("/music", require("./src/routes/music"));
+app.use("/public", require("./src/routes/public"));
 
 
 app.get("*", (req, res) => res.send({ error_code: 404 }))
 app.post("*", (req, res) => res.send({ error_code: 404 }))
 app.delete("*", (req, res) => res.send({ error_code: 404 }))
 
-app.listen(PORT, () =>
-  console.log(`Server started on: http://127.0.0.1:${PORT}`)
-);
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
